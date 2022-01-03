63310986cf3bf5eb6aaf3cfd69d5f89bb5f376cb
brianchien0417 <96788631+brianchien0417@users.noreply.github.com>
2022-01-03 18:04:51 -0400

    Update Boxes.js

--- src/client/Boxes.js
+++ src/client/Boxes.js
@@ -1,32 +1,97 @@
-import React from "react";
+import React, {useState, useEffect} from 'react';
 import { withRouter } from "react-router-dom";
 import './Client.css';
 
+function allTransactions(userEmail){ 
+  // localStorage.getcurrentPurchase("toMerch"), returns array of data that [{accepted: true, email: str, description: str, address: str }, {}, {}, {}]
+  // localstorage.getcurrentPurchase("") returns a string
+
+  if(localStorage.getItem("toMerch") === undefined || localStorage.getItem("toMerch") === null) {
+    var viableClients = [];
+  }
+  else viableClients = JSON.parse(localStorage.getItem("toMerch"));
+  var length = viableClients.length;
+  var noneOrPassed = [], transactionsProg = [], transactionsDone = [];
+
+  for(var i = 0; i < length; i++){
+    if(viableClients[i].email === userEmail){
+      if(viableClients[i].accepted === "none") noneOrPassed[i] = ("Product [" + (viableClients[i].description) + "]: Waiting for pickup point");
+      else if(viableClients[i].accepted === "passed") noneOrPassed.push("Product [" + viableClients[i].description + "]: Offer sent to merchant: waiting for acceptance");
+      else if(viableClients[i].accepted === "prog") transactionsProg.push("Product [" + viableClients[i].description + "]: Offer accepted by merchant, pick up from " + 
+                                                    viableClients[i].address + " upon delivery");
+      else if(viableClients[i].accepted === "done") transactionsDone.push("Product [" + viableClients[i].description + "]: Transaction completed");
+    }
+  }
+
+  var res = {nonePassed : noneOrPassed, prog : transactionsProg, done : transactionsDone};
+  if(!res) return "";
+  return res;
+}
+
 
 function Boxes(props) {
+
   const userEmail = props.location.state.login;
 
-  var allTransactions = (transactionName, index) => {
-    if(localStorage.getItem("toMerch") === undefined || localStorage.getItem("toMerch") === null) {
-      var viableClients = [];
+  // The SlideShow for Pending Transactions // 
+
+  var noneOrPassedres = allTransactions(userEmail)["nonePassed"];
+
+  const[noneOrPassedPurchases, setnoneOrPassedPurchases] = useState([]);
+  const[currentNonePurchase, setCurrentNonePurchase] = useState("");
+  const[previousNonePurchase, setPreviousNonePurchase] = useState("");
+  const[nextNonePurchase, setNextNonePurchase] = useState("");
+  const[currentNoneIndex, setCurrent] = useState(0);
+
+  useEffect(() => {
+    setnoneOrPassedPurchases(noneOrPassedres);
+    setCurrentNonePurchase(noneOrPassedres[currentNoneIndex]);
+    if (currentNoneIndex > 1){
+      setPreviousNonePurchase(noneOrPassedres[currentNoneIndex - 1]);
     }
-    else viableClients = JSON.parse(localStorage.getItem("toMerch"));
-    var length = viableClients.length;
-    
-    var noneOrPassed = [], transactionsProg = [], transactionsDone = [];
-    for(var i = 0; i < length; i++){
-      if(viableClients[i].email === userEmail){
-        if(viableClients[i].accepted === "none") noneOrPassed.push("Product " + viableClients[i].description + ": waiting for business");
-        else if(viableClients[i].accepted === "passed") noneOrPassed.push("Product " + viableClients[i].description + ": offer sent to merchant: waiting for acceptance");
-        else if(viableClients[i].accepted === "prog") transactionsProg.push("Product " + viableClients[i].description + ": offer accepted by merchant, pick up from " + 
-                                                      viableClients[i].address + " upon delivery");
-        else if(viableClients[i].accepted === "done") transactionsDone.push("Product " + viableClients[i].description + ": transaction completed");
+    else if (/* currentNoneIndex == 0 ||  */noneOrPassedPurchases.length <= 2){
+      setPreviousNonePurchase("");
+    }
+    else{
+      setPreviousNonePurchase(noneOrPassedres[noneOrPassedres.length - 1]);
+    }
+    if (currentNoneIndex === noneOrPassedres.length - 1){
+      setNextNonePurchase(noneOrPassedres[0]);
+    }
+    else if (currentNoneIndex == 0 && noneOrPassedPurchases.length == 1){
+      setNextNonePurchase("");
+    }
+    else{
+      setNextNonePurchase(noneOrPassedres[currentNoneIndex + 1]);
+    }
+  }, [currentNoneIndex]);
+
+  function previousNoneSlide(){
+    if (noneOrPassedPurchases.length > 3){
+      if (currentNoneIndex > 0){
+        setCurrent(currentNoneIndex - 1);
+      }
+      else{
+        setCurrent(noneOrPassedPurchases.length - 1);
       }
     }
-    var res = {nonePassed : noneOrPassed, prog : transactionsProg, done : transactionsDone}
-    if(!res[transactionName][index]) return "";
-    return res[transactionName][index];
   }
+
+  function nextNoneSlide(){
+    if (noneOrPassedPurchases.length > 3){
+      if (currentNoneIndex === noneOrPassedPurchases.length - 1){
+        setCurrent(0);
+      }
+      else{
+        setCurrent(currentNoneIndex + 1);
+      }
+    }
+  }
+  
+  // The SlideShow for Transactions in Progress // 
+
+  // The SlideShow for finished Transactions // 
+
   return (
     <div className="placeholder">
       <div className="placeholder">
@@ -36,47 +101,17 @@ function Boxes(props) {
         </div>
         {/* offer bar */}
         <div className="flex-container">
-          <span className="box">
-            <span className="text" id="productBox">{allTransactions("nonePassed", 0)}</span>
-          </span>
-          <span className="box">
-            <span className="text" id="productBox">{allTransactions("nonePassed", 1)}</span>
-          </span>
-          <span className="box">
-            <span className="text" id="productBox">{allTransactions("nonePassed", 2)}</span>
-          </span>
-        </div>
-        {/* header bar */}
-        <div>
-          <h>Transactions in Progress</h>
-        </div>
-        {/* offer bar */}
-        <div className="flex-container">
-          <span className="box">
-            <span className="text" id="productBox">{allTransactions("prog", 0)}</span>
-          </span>
-          <span className="box">
-            <span className="text" id="productBox">{allTransactions("prog", 1)}</span>
-          </span>
-          <span className="box">
-            <span className="text" id="productBox">{allTransactions("prog", 2)}</span>
-          </span>
-        </div>
-        {/* header bar */}
-        <div>
-          <h>Completed Transactions</h>
-        </div>
-        {/* offer bar */}
-        <div className="flex-container">
-          <span className="box">
-            <span className="text" id="productBox">{allTransactions("done", 0)}</span>
-          </span>
-          <span className="box">
-            <span className="text" id="productBox">{allTransactions("done", 1)}</span>
-          </span>
-          <span className="box">
-            <span className="text" id="productBox">{allTransactions("done", 2)}</span>
-          </span>
+          <span className="box1">
+            <span className = "text" id= "productBox">{previousNonePurchase}</span> 
+          </span>   
+          <span className="box1">
+            <span className = "text" id= "productBox">{currentNonePurchase}</span> 
+          </span>        
+          <span className="box1">
+            <span className = "text" id= "productBox">{nextNonePurchase}</span> 
+          </span>     
+          <a class="prev" onClick = {previousNoneSlide}>&#10094;</a>
+          <a class="next" onClick= {nextNoneSlide}>&#10095;</a>
         </div>
       </div>
     </div>
