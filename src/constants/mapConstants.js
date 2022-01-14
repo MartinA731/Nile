9e4bc954c875a183ec32feea484ba27ac8ba52c9
brianchien0417 <96788631+brianchien0417@users.noreply.github.com>
2022-01-14 17:53:54 -0500

    Delete mapConstants.js

--- src/constants/mapConstants.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
-import MapContainer from './client/Map.js'
-
-export default GoogleApiWrapper({
-    apiKey:'AIzaSyB8bb4Id3hNnAmNw_TCHtnR3ldteG1fztM'
-  })(MapContainer);
